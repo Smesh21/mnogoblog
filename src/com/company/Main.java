0089b53d364c126b452e78d66f4a89f3294f9b00
Nazarov <vnazarov@meta-devs.io>
2021-12-17 20:29:01 +0500

    lession1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        //System.out.println("hell yeah");
+        int value1 = 5;
+        int value2 = 0;
+        int sum = value1 + value2;
+        System.out.println("Сумма " + sum);
+        int multiply = value1 * value2;
+        System.out.println("Произведение " + multiply);
+        int difference = value1 - value2;
+        System.out.println("Разность " + difference);
+        Scanner scanner = new Scanner(System.in);
+        //String userInput = scanner.next();
+        String userInput = new Scanner(System.in).next();
+        System.out.println(userInput);
+        // write your code here
     }
 }
