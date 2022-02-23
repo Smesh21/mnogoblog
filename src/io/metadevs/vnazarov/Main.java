8fe941b76109f76099598a242d3b96e8f39f1562
Vnazarov <vnazarov@meta-devs.io>
2022-02-23 18:58:58 +0500

    Камень ножницы бумага

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -2,8 +2,37 @@ package io.metadevs.vnazarov;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        public static void main(String[] args) {
+            int petya = (int) (Math.random() * 3);
+            int vasya = (int) (Math.random() * 3);
+
+            if (petya == vasya) {
+                System.out.println("Ничья");
+            } else {
+
+            switch (petya){
+                case 1:
+                    if (vasya == 2) {
+                        System.out.println("Победил Вася");
+                    } else {
+                        System.out.println("Победил Петя");
+                    }
+                    break;
+                case 2:
+                    if (vasya == 3) {
+                        System.out.println("Победил Вася");
+                    } else {
+                        System.out.println("Победил Петя");
+                    }
+                    break;
+                case 3:
+                    if (vasya == 1) {
+                        System.out.println("Победил Вася");
+                    } else {
+                        System.out.println("Победил Петя");
+                    }
+                    break;
+            }
+           }
+        }
     }
-}
