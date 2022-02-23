8ee0d45c35dc9eea92dab6a4ed49cf72e926718f
Vnazarov <vnazarov@meta-devs.io>
2022-02-23 18:01:57 +0500

    Тест Хорошо Плохо

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -2,8 +2,26 @@ package io.metadevs.vnazarov;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        public static void main(String[] args) {
+            int first = (int) (Math.random() * 2);
+            int second = (int) (Math.random() * 2);
+            int third = (int) (Math.random() * 2);
+            int result = 0;
+
+            if (first == 1) {
+                System.out.println("Хорошо");
+            } else {
+                System.out.println("Плохо");
+            }
+            if (second == 1) {
+                System.out.println("Хорошо");
+            } else {
+                System.out.println("Плохо");
+            }
+            if (third == 1) {
+                System.out.println("Хорошо");
+            } else {
+                System.out.println("Плохо");
+            }
+        }
     }
-}
