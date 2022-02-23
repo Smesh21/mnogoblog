a4a5d24e1e82d2c2a14a963ea706b5582e48e4ea
Vnazarov <vnazarov@meta-devs.io>
2022-02-23 19:27:37 +0500

    Numbers

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,9 +1,32 @@
 package io.metadevs.vnazarov;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число от одного до пяти:");
+        int number = scan.nextInt();
+        scan.close();
+        switch (number) {
+            case 1:
+                System.out.println("Вы ввели число один");
+                break;
+            case 2:
+                System.out.println("Вы ввели число два");
+                break;
+            case 3:
+                System.out.println("Вы ввели число три");
+                break;
+            case 4:
+                System.out.println("Вы ввели число четыре");
+                break;
+            case 5:
+                System.out.println("Вы ввели число пять");
+                break;
+            default:
+                System.out.println("Неверное число");
+        }
     }
 }
