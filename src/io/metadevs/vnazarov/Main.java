53e2db087137fca548e7ad79620ecb6663a54fb8
Vnazarov <vnazarov@meta-devs.io>
2022-02-25 20:07:22 +0500

    FamilyAtHome

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,34 +1,21 @@
 package io.metadevs.vnazarov;
 
 public class Main {
-    private static int[] home = new int
-    home[3];
-    String[] parents = new parents[3];
-    parents[0]="Папа";
-    parents[1]="Мама";
-    parents[2]="Сын";
+
+    private static int[] home = new int[3];
 
     public static void main(String[] args) {
-        for (int n = 0; n > home.length; n++) {
+        String[] parents = new String[3];
+        parents[0]="Папа";
+        parents[1]="Мама";
+        parents[2]="Сын";
+        for (int n = 0; n < home.length; n++) {
             home[n] = (int) (Math.random() * 2);
             if (home[n] == 1) {
-                System.out.println(parents[n]" дома");
+                System.out.println(parents[n]+" дома");
             } else {
-                System.out.println(parents[n]" не дома");
+                System.out.println(parents[n]+" не дома");
             }
-
-
-//        int a = (int) (Math.random() * 2);
-//        int b = (int) (Math.random() * 2);
-//        int c = (int) (Math.random() * 2);
-//
-//        String k = a == 0 ? "Папа дома" : "Папы дома нет";
-//        String g = b == 0 ? "Мама дома" : "Мамы дома нет";
-//        String j = c == 0 ? "Сын дома" : "Сына дома нет";
-//
-//        System.out.println(k);
-//        System.out.println(g);
-//        System.out.println(j);
         }
     }
 }
