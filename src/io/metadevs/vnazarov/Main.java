5848fef6fb86f9f0225b546efaec24be8d388900
Vnazarov <vnazarov@meta-devs.io>
2022-02-25 19:40:27 +0500

    FamilyAtHome?

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,21 +1,21 @@
 package io.metadevs.vnazarov;
 
 public class Main {
-    private static int[] home = new int home[3];
-    String[] parents  = new parents[3];
-    parents[0] = "Папа";
-    parents[1] = "Мама";
-    parents[2] = "Сын";
+    private static int[] home = new int
+    home[3];
+    String[] parents = new parents[3];
+    parents[0]="Папа";
+    parents[1]="Мама";
+    parents[2]="Сын";
 
-    public static int main(String[] args) {
-        for (int n=0; n>home.length; n++) {
-            home[n]= (int) (Math.random() * 2);
+    public static void main(String[] args) {
+        for (int n = 0; n > home.length; n++) {
+            home[n] = (int) (Math.random() * 2);
             if (home[n] == 1) {
-                System.out.println(parents[n] " дома");
+                System.out.println(parents[n]" дома");
             } else {
-                System.out.println(parents[n] " не дома");
-        }
-
+                System.out.println(parents[n]" не дома");
+            }
 
 
 //        int a = (int) (Math.random() * 2);
@@ -29,5 +29,6 @@ public class Main {
 //        System.out.println(k);
 //        System.out.println(g);
 //        System.out.println(j);
+        }
     }
 }
