e03a224f292664cd39d700904fdc9261a217707c
Vnazarov <vnazarov@meta-devs.io>
2022-02-26 16:45:13 +0500

    1-10

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,9 +1,11 @@
 package io.metadevs.vnazarov;
 
 public class Main {
-
+private static int number = 0;
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        for (int i = 0; i < 10; i++) {
+number = number + i; 
+        }
+        System.out.println(number);
     }
 }
