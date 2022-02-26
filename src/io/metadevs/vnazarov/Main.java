245ccccc8f0f54f7e7bf17de9f160ba3d5de0742
Vnazarov <vnazarov@meta-devs.io>
2022-02-26 16:58:44 +0500

    Number squares

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,9 +1,20 @@
 package io.metadevs.vnazarov;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число от 1 до 10:");
+        int userInput = sc.nextInt();
+        sc.close();
+        if (userInput < 10 & userInput > 1) {
+            for (int i = 1; i < userInput+1; i++) {
+                System.out.println("Квадрат " + i + " равен " + (i * i));
+        }
+        } else {
+System.out.println("Введено некоректное число");
+        }
     }
 }
