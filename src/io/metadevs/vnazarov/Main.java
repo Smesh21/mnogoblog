4f23fb5d7752aaff268aabeca4335fdac13c9112
Vnazarov <vnazarov@meta-devs.io>
2022-02-27 17:31:58 +0500

    GuessNumber

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,9 +1,35 @@
 package io.metadevs.vnazarov;
 
-public class Main {
+import java.util.Scanner;
+import java.lang.Math;
+
+class GameGuessNumber {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        int unknownNumber = (int) Math.floor(Math.random() * 10);
+        int NUMTRYINGS = 3;
+        boolean gameIsWon = false;
+        Scanner input = new Scanner(System.in);
+        System.out.println("ЗАДАЧА: Угадать число (от 0 до 100) за 10 попыток");
+        for (int i = 0; i < NUMTRYINGS; i++) {
+            System.out.print("Введите ваше число: ");
+            int userNumber = input.nextInt();
+            if (userNumber > unknownNumber) {
+                System.out.println("Моё число меньше.");
+            }
+            else if (userNumber < unknownNumber) {
+                System.out.println("Моё число больше.");
+            }
+            else {
+                gameIsWon = true;
+                break;
+            }
+        }
+        if (gameIsWon) {
+            System.out.println("Игра выиграна");
+        }
+        else {
+            System.out.println("Игра проиграна. Загаданное число " + unknownNumber);
+        }
     }
 }
