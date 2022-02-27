d22fb3fe61b6de55cd34e6fcabd8b9e1332e23da
Vnazarov <vnazarov@meta-devs.io>
2022-02-27 17:59:09 +0500

    Grades

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,17 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+
+        String[] students = { "Вася", "Петя", "Маша", "Оля", "Коля" };
+
+        int[] grade = new int[5];
+
+        for (int i = 0; i < grade.length; i++) {
+            grade[i] = (int) (Math.random() * 10 + 1);
+        }
+        System.out.println("Оценки учеников:");
+        for (int i = 0; i < grade.length; i++) {
+            System.out.println(students[i] + " " + grade[i]);
+        }
     }
 }
