704d852c6c722e3c39afa7036455cc47f4dd81ce
Vnazarov <vnazarov@meta-devs.io>
2022-03-07 16:48:02 +0500

    square root

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -1,9 +1,22 @@
 package io.metadevs.vnazarov;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        System.out.println("Введите число 1:");
+        num1 = sc.nextInt();
+        System.out.println("Введите число 2:");
+        num2 = sc.nextInt();
+        sc.close();
+        double sqrtNum1;
+        double sqrtNum2;
+        sqrtNum1 = Math.sqrt(num1);
+        sqrtNum2 = Math.sqrt(num2);
+        System.out.println("Корень квадратный " +num1 +" = " +sqrtNum1);
+        System.out.println("Корень квадратный " +num2 +" = " +sqrtNum2);
     }
 }
