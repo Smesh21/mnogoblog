6dd85af4dc3383eb45a52b7637e0ba56569ebc0c
Vnazarov <vnazarov@meta-devs.io>
2022-03-07 14:48:44 +0500

    arrays

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,14 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        String array[][] = {{"Зима", "Весна", "Лето", "Осень"},
+                {"Папа", "Мама", "Сын", "Дочь"},
+                {"Холодно", "Тепло", "Жарко", "Комфортно"}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
