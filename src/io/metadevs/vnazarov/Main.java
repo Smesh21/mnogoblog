41bdd56b59d995862b13483f21bebaf3c48a07dd
Vnazarov <vnazarov@meta-devs.io>
2022-03-07 14:05:29 +0500

    RandomArray

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -6,23 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.println("Введите число от 1 до 10:");
         int userInput = sc.nextInt();
         sc.close();
-        if (userInput < 1 || userInput > 10) {
-            System.out.println("Неверное число");
-        } else {
+        if (userInput > 0 && userInput < 11) {
             int[] array = new int[userInput];
 
             for (int i = 0; i < userInput; i++) {
                 array[i] = (int) (Math.random() * 10 + 1);
                 System.out.print(array[i]);
-                if (i == (userInput - 1)) {
-                    System.out.print(".");
-                } else {
+                if (i != (userInput - 1)) {
                     System.out.print(", ");
+                } else {
+                    System.out.print(".");
                 }
             }
-        }
-    }
+        } else {
+                System.out.println("Неверное число");
+            }
+}
 }
