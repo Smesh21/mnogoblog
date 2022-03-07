dae00572c939b9df7f79dc4bdde8dab1bcf89377
Vnazarov <vnazarov@meta-devs.io>
2022-03-07 13:32:16 +0500

    sumNum1-10

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,7 @@ package io.metadevs.vnazarov;
 public class Main {
 private static int number = 0;
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 11; i++) {
 number = number + i; 
         }
         System.out.println(number);
