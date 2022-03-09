fd7376b1951e271b39a42d9a6241016148177c8c
Vnazarov <vnazarov@meta-devs.io>
2022-03-09 20:54:52 +0500

    CheckUserInput

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,13 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+        String text = "Папа дома, мама дома, сын дома, дочь дома.";
+        System.out.println(text);
+        String replaceText1 = text.replace("дома", "в школе");
+        System.out.println(replaceText1);
+        String replaceText2 = replaceText1.replace("сын", "Ваня");
+        System.out.println(replaceText2);
+        String replaceFinal = replaceText2.replace("дочь", "Оля");
+        System.out.println(replaceFinal);
     }
 }
