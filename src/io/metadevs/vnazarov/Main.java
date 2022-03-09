6d554f95ecadb635c30adf99402eb5f5b22ca08c
Vnazarov <vnazarov@meta-devs.io>
2022-03-09 21:32:12 +0500

    ?

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,22 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+
+        String animal1 = "коза";
+        String animal1name = "Катя";
+        int animal1Age = 3;
+
+        String animal2 = "корова";
+        String animal2Name = "Зорька";
+        int animal2Age = 4;
+
+        String animal3 = "свинка";
+        String animal3Name = "Мотя";
+        int animal3Age = 2;
+
+        System.out.println("У бабушки живут:");
+        System.out.println(animal1 +" " +animal1name +" " +animal1Age +" года");
+        System.out.println(animal2 +" " +animal2Name +" " +animal2Age +" года");
+        System.out.println(animal3 +" " +animal3Name +" " +animal3Age +" года");
     }
 }
