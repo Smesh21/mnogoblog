7580fb354a9026f75bfbea3c66e761cfd95e8865
Vnazarov <vnazarov@meta-devs.io>
2022-03-09 21:46:30 +0500

    StudentsMediumMark

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,7 +3,32 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hell yeah");
-	// write your code here
+            int mark1 = (int) (Math.random() * 10) + 1;
+            int mark2 = (int) (Math.random() * 10) + 1;
+            int mark3 = (int) (Math.random() * 10) + 1;
+            double mediumMarks;
+
+            Stud student1 = new Stud();
+            student1.name = "Вася ";
+            student1.mark = mark1;
+
+            Stud student2 = new Stud();
+            student2.name = "Петя ";
+            student2.mark = mark2;
+
+            Stud student3 = new Stud();
+            student3.name = "Саша ";
+            student3.mark = mark3;
+
+            mediumMarks = (double) (student1.mark + student2.mark + student3.mark) / 3;
+
+            System.out.println(student1.name + student1.mark);
+            System.out.println(student2.name + student2.mark);
+            System.out.println(student3.name + student3.mark);
+            System.out.println("Средняя оценка:" + mediumMarks);
+    }
+    public static class Stud {
+        String name;
+        int mark;
     }
 }
