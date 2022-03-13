68ce644e6da7fbd810620c5750c7d7f12dc8c0fd
Vnazarov <vnazarov@meta-devs.io>
2022-03-13 19:40:45 +0500

    TestGoodOrBad

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,24 +3,14 @@ package io.metadevs.vnazarov;
 public class Main {
 
         public static void main(String[] args) {
-            int first = (int) (Math.random() * 10+1);
-            int second = (int) (Math.random() * 10+1);
-            int third = (int) (Math.random() * 10+1);
-
-            if (first > 4) {
-                System.out.println("Хорошо");
-            } else {
-                System.out.println("Плохо");
+            int testCAP = 3;
+            for (int i = 0; i < testCAP; i++) {
+                int test = (int) (Math.random() * 10+1);
+                if (test > 4) {
+                    System.out.println("Хорошо");
+                } else {
+                    System.out.println("Плохо");
             }
-            if (second > 4) {
-                System.out.println("Хорошо");
-            } else {
-                System.out.println("Плохо");
-            }
-            if (third == 4) {
-                System.out.println("Хорошо");
-            } else {
-                System.out.println("Плохо");
             }
         }
     }
