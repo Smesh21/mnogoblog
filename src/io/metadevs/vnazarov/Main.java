4a9b25075f0cbe0c2107863b293a959752a5c7ed
Vnazarov <vnazarov@meta-devs.io>
2022-03-17 21:36:18 +0500

    livecoding2

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -6,15 +6,45 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число от 1 до 10:");
+        System.out.println("Введите число от 1 до 99:");
         int userInput = sc.nextInt();
         sc.close();
-        if (userInput < 10 & userInput > 1) {
-            for (int i = 1; i < userInput+1; i++) {
-                System.out.println("Квадрат " + i + " равен " + (i * i));
-        }
+        if (userInput < 100 & userInput > 0) {
+            switch (userInput / 10) {
+                case 0:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    switch (userInput % 10) {
+                        case 0:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9:
+                            System.out.println("Мне " + userInput + " лет");
+                            break;
+                        case 1:
+                            System.out.println("Мне " + userInput + " год");
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            System.out.println("Мне " + userInput + " года");
+                            break;
+                    }
+                    break;
+                case 1:
+                    System.out.println("Мне " + userInput + " лет");
+                    break;
+            }
         } else {
-System.out.println("Введено некоректное число");
+            System.out.println("Введено некоректное число");
         }
     }
 }
