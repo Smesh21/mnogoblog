5b52d979df8a0ded400d892a413e1538c4a503b9
Vnazarov <vnazarov@meta-devs.io>
2022-03-23 15:11:21 +0500

    TestGoodOrBad

--- src/io/metadevs/vnazarov/Stud.java
+++ src/io/metadevs/vnazarov/Stud.java
@@ -3,7 +3,7 @@ package io.metadevs.vnazarov;
 public class Stud {
 
     public Stud() {
-        this.mark = (int) (Math.random() * 10) + 1;;
+        this.mark = (int) (Math.random() * 10) + 1;
     }
     String name;
     int mark;
