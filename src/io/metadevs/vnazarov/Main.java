cd77d669129d194162ad2d991032cde999db71cd
Vnazarov <vnazarov@meta-devs.io>
2022-03-23 15:10:19 +0500

    TestGoodOrBad

--- src/io/metadevs/vnazarov/Main.java
+++ src/io/metadevs/vnazarov/Main.java
@@ -3,23 +3,17 @@ package io.metadevs.vnazarov;
 public class Main {
 
     public static void main(String[] args) {
-            int mark1 = (int) (Math.random() * 10) + 1;
-            int mark2 = (int) (Math.random() * 10) + 1;
-            int mark3 = (int) (Math.random() * 10) + 1;
-            double mediumMarks;
 
             Stud student1 = new Stud();
             student1.name = "Вася ";
-            student1.mark = mark1;
 
             Stud student2 = new Stud();
             student2.name = "Петя ";
-//            student2.mark = mark2;
 
             Stud student3 = new Stud();
+            student3.name = "Саша ";
 
-
-            mediumMarks = (double) (student1.mark + student2.mark + student3.mark) / 3;
+            double mediumMarks = (double) (student1.mark + student2.mark + student3.mark) / 3;
 
             System.out.println(student1.name + student1.mark);
             System.out.println(student2.name + student2.mark);
--- src/io/metadevs/vnazarov/Stud.java
+++ src/io/metadevs/vnazarov/Stud.java
@@ -1,12 +1,10 @@
 package io.metadevs.vnazarov;
 
 public class Stud {
-    public Stud(String name, int mark) {
-        this.name = name;
+    public Stud(int mark) {
         this.mark = mark;
     }
     public Stud() {
-        this.name = "defaultName ";
         this.mark = (int) (Math.random() * 10) + 1;;
     }
     String name;
